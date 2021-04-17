a4284af6c93557c1588d8d4608e04fe66ab47b2c
Your Name <you@example.com>
2021-04-17 12:01:31 -0400

    test

--- firmware/flash_apimote.sh
+++ firmware/flash_apimote.sh
@@ -4,4 +4,5 @@ FW_FILE='apimotev4_gf.hex'
 
 echo "Flashing $FW_FILE to the connected USB serial device."
 
+#Removed hardcoded serial baud for APIMote PCB
 board=apimote3 ./goodfet.bsl -e -p $FW_FILE
